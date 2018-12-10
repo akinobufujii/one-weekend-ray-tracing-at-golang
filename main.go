19bc4036619fcb9ba6a557b576d2c6edb4c295af
akinobufujii <aki.develop@gmail.com>
2018-12-10 23:37:27 +0900

    tgaグラデーションを出力するサンプルを作成

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+
+	"github.com/ftrvxmtrx/tga"
+)
+
+func main() {
+	fmt.Println("start")
+
+	width := 200
+	height := 100
+
+	outputImage := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			// Yは逆転しているので反対から書いていく
+			outputImage.SetRGBA(x, height-y-1, color.RGBA{uint8(float32(x) / float32(width) * 255.99), uint8(float32(y) / float32(height) * 255.99), uint8(255 * 0.2), 255})
+		}
+	}
+
+	file, _ := os.Create("result.tga")
+	defer file.Close()
+
+	// tgaで出力
+	if err := tga.Encode(file, outputImage); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("end")
+}
